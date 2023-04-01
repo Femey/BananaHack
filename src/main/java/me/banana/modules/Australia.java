84ad4e44d17dd24ce1f5ddd4f887967b64d6488a
spaz <81682441+spazmilk@users.noreply.github.com>
2023-04-01 19:07:34 +1100

    updated

--- src/main/java/me/banana/modules/Australia.java
+++ src/main/java/me/banana/modules/Australia.java
@@ -1,54 +1,76 @@
 package me.banana.modules;
 
 import me.banana.BananaHack;
+import meteordevelopment.orbit.EventHandler;
+import meteordevelopment.meteorclient.events.game.GameJoinedEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import net.minecraft.client.gl.PostProcessShader;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
+import java.io.IOException;
+
 public class Australia extends Module {
     private PostProcessShader shader;
     private static final Identifier SHADER_LOCATION = new Identifier("shaders/post/flip.json");
 
+    private final Setting<Boolean> fire = new BoolSetting.Builder()
+        .name("set-on-fire")
+        .description("Sets you on fire.")
+        .defaultValue(true)
+        .build();
+
     public Australia() {
-        super(BananaHack.BANANATROLL, "Australia", "Makes you Australian");
+        super(BananaHack.BANANATROLL, "Australia", "Makes you Australian.");
     }
 
     @Override
-    public void onEnable() {
+    public void onActivate() {
         if (mc.getResourceManager() == null) return;
 
-        shader = new PostProcessShader(mc.getResourceManager(), SHADER_LOCATION, mc.getFramebuffer(), mc.getFramebufferWidth(), mc.getFramebufferHeight());
-        mc.getFramebuffer().Shader(shader);
-        assert mc.player != null;
-        mc.player.setFireTicks(1);
+        shader = new PostProcessShader(mc.getResourceManager(), SHADER_LOCATION, mc.getFramebuffer(), mc.getWindow().getFramebufferWidth(), mc.getWindow().getFramebufferHeight());
+        mc.getFramebuffer().setShader(shader);
+        if (fire.get()) {
+            assert mc.player != null;
+            mc.player.setOnFireFor(1);
+        }
     }
 
     @Override
-    public void onDisable() {
+    public void onDeactivate() {
         if (shader != null) {
             shader.close();
             shader = null;
         }
     }
 
-    @Override
-    public void onResourceManagerReload(ResourceManager manager) {
-        if (shader != null) {
-            shader.close();
-            shader = new PostProcessShader(manager, SHADER_LOCATION, mc.getFramebuffer(), mc.getFramebufferWidth(), mc.getFramebufferHeight());
-        }
+    @EventHandler
+    private void onGameJoined(GameJoinedEvent event) {
+        onActivate();
     }
 
     @Override
-    public void onUpdate() {
+    public void onResourceManagerReload(ResourceManager manager) throws IOException {
+        if (shader != null) {
+            shader.close();
+            shader = new PostProcessShader(manager, SHADER_LOCATION, mc.getFramebuffer(), mc.getWindow().getFramebufferWidth(), mc.getWindow().getFramebufferHeight());
+        }
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
         if (!(mc.getCameraEntity() instanceof AbstractClientPlayerEntity)) return;
 
         if (shader != null) {
-            shader.render(mc.getTickDelta());
+            shader.render(event.delta);
         }
-        assert mc.player != null;
-        mc.player.setFireTicks(1);
+        if (fire.get()) {
+            assert mc.player != null;
+            mc.player.setOnFireFor(1);
+        }
     }
 }
