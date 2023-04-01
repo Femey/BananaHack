fe3131f0316710db821b6800f4af5884159f2b43
spaz <81682441+spazmilk@users.noreply.github.com>
2023-04-01 20:08:45 +1100

    Update BananaRPC.java

--- src/main/java/me/banana/modules/BananaRPC.java
+++ src/main/java/me/banana/modules/BananaRPC.java
@@ -1,37 +1,311 @@
 package me.banana.modules;
 
-import club.minnced.discord.rpc.*;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ClientModInitializer;
+import me.banana.BananaHack;
+import meteordevelopment.discordipc.DiscordIPC;
+import meteordevelopment.discordipc.RichPresence;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.events.game.OpenScreenEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.WidgetScreen;
+import meteordevelopment.meteorclient.gui.utils.StarscriptTextBoxRenderer;
+import meteordevelopment.meteorclient.gui.widgets.WWidget;
+import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.Utils;
+import meteordevelopment.meteorclient.utils.misc.MeteorStarscript;
+import meteordevelopment.orbit.EventHandler;
+import meteordevelopment.starscript.Script;
+import net.minecraft.client.gui.screen.*;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.screen.option.*;
+import net.minecraft.client.gui.screen.pack.PackScreen;
+import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import net.minecraft.client.gui.screen.world.EditGameRulesScreen;
+import net.minecraft.client.gui.screen.world.EditWorldScreen;
+import net.minecraft.client.gui.screen.world.SelectWorldScreen;
+import net.minecraft.client.realms.gui.screen.RealmsScreen;
+import net.minecraft.util.Pair;
+import net.minecraft.util.Util;
 
-public class BananaRPC implements ClientModInitializer {
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final String CLIENT_ID = "Your Discord Application Client ID Here";
-    private static final String LARGE_IMAGE_KEY = "your_large_image_key";
-    private static final String LARGE_IMAGE_TEXT = "Your Large Image Text";
-    private static final String SMALL_IMAGE_KEY = "your_small_image_key";
-    private static final String SMALL_IMAGE_TEXT = "Your Small Image Text";
+public class DiscordPresence extends Module {
+    public enum SelectMode {
+        Random,
+        Sequential
+    }
+
+    private final SettingGroup sgLine1 = settings.createGroup("Line 1");
+    private final SettingGroup sgLine2 = settings.createGroup("Line 2");
+
+    // Line 1
+
+    private final Setting<List<String>> line1Strings = sgLine1.add(new StringListSetting.Builder()
+        .name("line-1-messages")
+        .description("Messages used for the first line.")
+        .defaultValue("{player}", "{server}")
+        .onChanged(strings -> recompileLine1())
+        .renderer(StarscriptTextBoxRenderer.class)
+        .build()
+    );
+
+    private final Setting<Integer> line1UpdateDelay = sgLine1.add(new IntSetting.Builder()
+        .name("line-1-update-delay")
+        .description("How fast to update the first line in ticks.")
+        .defaultValue(200)
+        .min(10)
+        .sliderRange(10, 200)
+        .build()
+    );
+
+    private final Setting<SelectMode> line1SelectMode = sgLine1.add(new EnumSetting.Builder<SelectMode>()
+        .name("line-1-select-mode")
+        .description("How to select messages for the first line.")
+        .defaultValue(SelectMode.Sequential)
+        .build()
+    );
+
+    // Line 2
+
+    private final Setting<List<String>> line2Strings = sgLine2.add(new StringListSetting.Builder()
+        .name("line-2-messages")
+        .description("Messages used for the second line.")
+        .defaultValue("Do you want a banana?", "{round(server.tps, 1)} TPS", "Playing on {server.difficulty} difficulty.", "{server.player_count} Players online")
+        .onChanged(strings -> recompileLine2())
+        .renderer(StarscriptTextBoxRenderer.class)
+        .build()
+    );
+
+    private final Setting<Integer> line2UpdateDelay = sgLine2.add(new IntSetting.Builder()
+        .name("line-2-update-delay")
+        .description("How fast to update the second line in ticks.")
+        .defaultValue(60)
+        .min(10)
+        .sliderRange(10, 200)
+        .build()
+    );
+
+    private final Setting<SelectMode> line2SelectMode = sgLine2.add(new EnumSetting.Builder<SelectMode>()
+        .name("line-2-select-mode")
+        .description("How to select messages for the second line.")
+        .defaultValue(SelectMode.Sequential)
+        .build()
+    );
+
+    private static final RichPresence rpc = new RichPresence();
+    private SmallImage currentSmallImage;
+    private int ticks;
+    private boolean forceUpdate, lastWasInMainMenu;
+
+    private final List<Script> line1Scripts = new ArrayList<>();
+    private int line1Ticks, line1I;
+
+    private final List<Script> line2Scripts = new ArrayList<>();
+    private int line2Ticks, line2I;
+
+    public static final List<Pair<String, String>> customStates = new ArrayList<>();
+
+    static {
+        registerCustomState("com.terraformersmc.modmenu.gui", "Browsing mods");
+        registerCustomState("me.jellysquid.mods.sodium.client", "Changing options");
+    }
+
+    public DiscordPresence() {
+        super(Categories.Misc, "discord-presence", "Displays Banana as your presence on Discord.");
+
+        runInMainMenu = true;
+    }
+
+    /** Registers a custom state to be used when the current screen is a class in the specified package. */
+    public static void registerCustomState(String packageName, String state) {
+        for (var pair : customStates) {
+            if (pair.getLeft().equals(packageName)) {
+                pair.setRight(state);
+                return;
+            }
+        }
+
+        customStates.add(new Pair<>(packageName, state));
+    }
+
+    /** The package name must match exactly to the one provided through {@link #registerCustomState(String, String)}. */
+    public static void unregisterCustomState(String packageName) {
+        customStates.removeIf(pair -> pair.getLeft().equals(packageName));
+    }
 
     @Override
-    public void onInitializeClient() {
-        DiscordEventHandlers handlers = new DiscordEventHandlers();
-        DiscordRPC.INSTANCE.Discord_Initialize(CLIENT_ID, handlers, true, "");
+    public void onActivate() {
+        DiscordIPC.start(835240968533049424L, null);
 
-        DiscordRichPresence discordPresence = new DiscordRichPresence();
-        discordPresence.largeImageKey = LARGE_IMAGE_KEY;
-        discordPresence.largeImageText = LARGE_IMAGE_TEXT;
-        discordPresence.smallImageKey = SMALL_IMAGE_KEY;
-        discordPresence.smallImageText = SMALL_IMAGE_TEXT;
+        rpc.setStart(System.currentTimeMillis() / 1000L);
 
-        DiscordRPC.INSTANCE.Discord_UpdatePresence(discordPresence);
+        String largeText = "%s %s".formatted(BananaHack.BANANAHACK, BananaHack."0.4");
+        if (!MeteorClient.DEV_BUILD.isEmpty()) largeText += " Dev Build: " + MeteorClient.DEV_BUILD;
+        rpc.setLargeImage("meteor_client", largeText);
 
-        new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                DiscordRPC.INSTANCE.Discord_RunCallbacks();
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException ignored) {}
+        currentSmallImage = SmallImage.Snail;
+
+        recompileLine1();
+        recompileLine2();
+
+        ticks = 0;
+        line1Ticks = 0;
+        line2Ticks = 0;
+        lastWasInMainMenu = false;
+
+        line1I = 0;
+        line2I = 0;
+    }
+
+    @Override
+    public void onDeactivate() {
+        DiscordIPC.stop();
+    }
+
+    private void recompile(List<String> messages, List<Script> scripts) {
+        scripts.clear();
+
+        for (String message : messages) {
+            Script script = MeteorStarscript.compile(message);
+            if (script != null) scripts.add(script);
+        }
+
+        forceUpdate = true;
+    }
+
+    private void recompileLine1() {
+        recompile(line1Strings.get(), line1Scripts);
+    }
+
+    private void recompileLine2() {
+        recompile(line2Strings.get(), line2Scripts);
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        boolean update = false;
+
+        // Image
+        if (ticks >= 200 || forceUpdate) {
+            currentSmallImage = currentSmallImage.next();
+            currentSmallImage.apply();
+            update = true;
+
+            ticks = 0;
+        }
+        else ticks++;
+
+        if (Utils.canUpdate()) {
+            // Line 1
+            if (line1Ticks >= line1UpdateDelay.get() || forceUpdate) {
+                if (line1Scripts.size() > 0) {
+                    int i = Utils.random(0, line1Scripts.size());
+                    if (line1SelectMode.get() == SelectMode.Sequential) {
+                        if (line1I >= line1Scripts.size()) line1I = 0;
+                        i = line1I++;
+                    }
+
+                    String message = MeteorStarscript.run(line1Scripts.get(i));
+                    if (message != null) rpc.setDetails(message);
+                }
+                update = true;
+
+                line1Ticks = 0;
+            } else line1Ticks++;
+
+            // Line 2
+            if (line2Ticks >= line2UpdateDelay.get() || forceUpdate) {
+                if (line2Scripts.size() > 0) {
+                    int i = Utils.random(0, line2Scripts.size());
+                    if (line2SelectMode.get() == SelectMode.Sequential) {
+                        if (line2I >= line2Scripts.size()) line2I = 0;
+                        i = line2I++;
+                    }
+
+                    String message = MeteorStarscript.run(line2Scripts.get(i));
+                    if (message != null) rpc.setState(message);
+                }
+                update = true;
+
+                line2Ticks = 0;
+            } else line2Ticks++;
+        }
+        else {
+            if (!lastWasInMainMenu) {
+                rpc.setDetails(MeteorClient.NAME + " " + (MeteorClient.DEV_BUILD.isEmpty() ? MeteorClient.VERSION : MeteorClient.VERSION + " " + MeteorClient.DEV_BUILD));
+
+                if (mc.currentScreen instanceof TitleScreen) rpc.setState("Looking at title screen");
+                else if (mc.currentScreen instanceof SelectWorldScreen) rpc.setState("Selecting world");
+                else if (mc.currentScreen instanceof CreateWorldScreen || mc.currentScreen instanceof EditGameRulesScreen) rpc.setState("Creating world");
+                else if (mc.currentScreen instanceof EditWorldScreen) rpc.setState("Editing world");
+                else if (mc.currentScreen instanceof LevelLoadingScreen) rpc.setState("Loading world");
+                else if (mc.currentScreen instanceof MultiplayerScreen) rpc.setState("Selecting server");
+                else if (mc.currentScreen instanceof AddServerScreen) rpc.setState("Adding server");
+                else if (mc.currentScreen instanceof ConnectScreen || mc.currentScreen instanceof DirectConnectScreen) rpc.setState("Connecting to server");
+                else if (mc.currentScreen instanceof WidgetScreen) rpc.setState("Browsing Meteor's GUI");
+                else if (mc.currentScreen instanceof OptionsScreen || mc.currentScreen instanceof SkinOptionsScreen || mc.currentScreen instanceof SoundOptionsScreen || mc.currentScreen instanceof VideoOptionsScreen || mc.currentScreen instanceof ControlsOptionsScreen || mc.currentScreen instanceof LanguageOptionsScreen || mc.currentScreen instanceof ChatOptionsScreen || mc.currentScreen instanceof PackScreen || mc.currentScreen instanceof AccessibilityOptionsScreen) rpc.setState("Changing options");
+                else if (mc.currentScreen instanceof CreditsScreen) rpc.setState("Reading credits");
+                else if (mc.currentScreen instanceof RealmsScreen) rpc.setState("Browsing Realms");
+                else {
+                    boolean setState = false;
+                    if (mc.currentScreen != null) {
+                        String className = mc.currentScreen.getClass().getName();
+                        for (var pair : customStates) {
+                            if (className.startsWith(pair.getLeft())) {
+                                rpc.setState(pair.getRight());
+                                setState = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!setState) rpc.setState("In main menu");
+                }
+
+                update = true;
             }
-        }, "RPC-Callback-Handler").start();
+        }
+
+        // Update
+        if (update) DiscordIPC.setActivity(rpc);
+        forceUpdate = false;
+        lastWasInMainMenu = !Utils.canUpdate();
+    }
+
+    @EventHandler
+    private void onOpenScreen(OpenScreenEvent event) {
+        if (!Utils.canUpdate()) lastWasInMainMenu = false;
+    }
+
+    @Override
+    public WWidget getWidget(GuiTheme theme) {
+        WButton help = theme.button("Open documentation.");
+        help.action = () -> Util.getOperatingSystem().open("https://github.com/MeteorDevelopment/meteor-client/wiki/Starscript");
+
+        return help;
+    }
+
+    private enum SmallImage {
+        Wobblybananahat("Wobblybananahat", "Wobblybananahat"),
+        spaz("spaz", "spaz");
+
+        private final String key, text;
+
+        SmallImage(String key, String text) {
+            this.key = key;
+            this.text = text;
+        }
+
+        void apply() {
+            rpc.setSmallImage(key, text);
+        }
+
+        SmallImage next() {
+            if (this == MineGame) return Snail;
+            return MineGame;
+        }
     }
 }
